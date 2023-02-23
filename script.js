6f3281704c1a87030572f08cd0b0cfd7769928e4
Italo souza sampaio <italo12346@gmail.com>
2023-02-23 13:04:35 -0300

    implementação de slides

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const checkBox = document.querySelector('#dark-mode')
 const page = document.querySelector('body')
+const container = document.querySelector('.container')
 let cont = 0
 checkBox.addEventListener('change', () => {
     if (cont == 0) {
         cont++;
         console.log(cont);
         page.style.cssText =
-            'background-color: #000;'
-    }else{
-        cont=0;
+            'background-color: #000;' +
+            'color:#fff;'
+        container.style.cssText =
+            'background-color: #274360 ;'
+    } else {
+        cont = 0;
         console.log(cont);
         page.style.cssText =
-            'background-color: #fff;'
+            'background-color: #fff;'+
+            'color:#000'
+            ;
+        container.style.cssText =
+            'background-color: antiquewhite;'
+            ;
     }
 })
